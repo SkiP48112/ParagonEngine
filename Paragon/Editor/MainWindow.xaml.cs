0b729ade572cd1928420dadacfe55705270f3ca2
kbazilenko <skip48112@yandex.ru>
2025-01-20 18:53:40 +0300

    Setuped engine and editor projects. Updated VS settings, working on projects browsing window

---
+++ Paragon/Editor/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Editor.GameProject;
+using System.Windows;
+
+namespace Editor
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += OnMainWindowLoaded;
+        }
+
+        private void OnMainWindowLoaded(object sender, RoutedEventArgs e)
+        {
+            Loaded -= OnMainWindowLoaded;
+            OpenProjectBrowsingDialog();
+        }
+
+        private void OpenProjectBrowsingDialog()
+        {
+            var projectBrowsingDialog = new ProjectBrowsingDialog();
+            if (projectBrowsingDialog.ShowDialog() == false){
+                Application.Current.Shutdown();
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
